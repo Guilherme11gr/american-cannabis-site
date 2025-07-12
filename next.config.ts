493277467ba1baf557679675de734e82edc048dc
Guilherme Revoredo <guilherme11.gr@gmail.com>
2025-07-12 14:42:52 -0300

    chore: ajuste no deploy

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,11 @@ const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   assetPrefix: assetPrefix,
-  images: { unoptimized: true },
+  images: {
+    loader: 'akamai',    // usa o path abaixo
+    path: assetPrefix, // prefixo absoluto para todas as <Image>
+    unoptimized: true,
+  },
   compiler: {
     emotion: true,
   },
