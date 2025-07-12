febfc42c180ab0322f0df6b5056768f6e567e4a0
Guilherme Revoredo <guilherme11.gr@gmail.com>
2025-07-12 14:07:25 -0300

    chore: ajuste de build

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
+// const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/american-cannabis-site',
+  assetPrefix: '/american-cannabis-site/',
   images: { unoptimized: true },
   compiler: {
-    // ativa o transform do Emotion no SWC
     emotion: true,
   },
 };
