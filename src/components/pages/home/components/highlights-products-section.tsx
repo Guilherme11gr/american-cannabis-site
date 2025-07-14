bbbe3528d521bb42d95adf023c353783a6e2a0ca
Guilherme Revoredo <guilherme11.gr@gmail.com>
2025-07-14 16:13:35 -0300

    aa

--- src/components/pages/home/components/highlights-products-section.tsx
+++ src/components/pages/home/components/highlights-products-section.tsx
@@ -106,7 +106,7 @@ export const HighlightsProductsSection: React.FC<{ featuredProducts: ProductSumm
         {featuredProducts.map((product, idx) => (
           <li className="keen-slider__slide" key={idx}>
             <Link href={`/products/${product.slug}`} className="product-link">
-              <Image src={`${product.mainPhoto?.image?.slice(1) ?? 'imgs/sys/not-found.jpg'}`} width={277} height={277} alt={product.name} />
+              <Image src={product.images?.[0].image.slice(1) ?? 'imgs/sys/not-found.jpg'} width={277} height={277} alt={product.name} />
               <h3>{product.name}</h3>
               <h2>{toCurrency(product.price)}</h2>
               <Button>Ver Detalhes</Button>
